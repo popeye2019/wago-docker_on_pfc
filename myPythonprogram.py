08ffc2b60f77684a43c644f937de682d9e5c750e
dannyolsen <dannyolsen1980@gmail.com>
2022-05-25 11:04:07 +0200

    program is done - upload to docker hub

---
+++ myPythonprogram.py
@@ -0,0 +1,10 @@
+import os
+os.system('clear')          #clear terminal screen
+
+print("Hello world! This program is running!! :)")
+
+name = input("What's your name my friend? ")
+
+print(f'Hello {name}, I hope you learn something from this tutorial :)')
+
+print("This program has ended...")
