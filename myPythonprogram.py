16dc9df4402dcc8a3261c43076bbf1a700d2dcc4
Danny <17601115+dannyolsen@users.noreply.github.com>
2022-05-25 11:26:12 +0200

    pythonprogram done

--- myPythonprogram.py
+++ myPythonprogram.py
@@ -1,6 +1,13 @@
 import os
-os.system('clear')          #clear terminal screen
 
+#Detecting operating system 
+if(os.name == 'nt') :      #windows
+    os.system('cls')          #clear terminal screen
+
+elif(os.name == 'posix'):  #linux
+    os.system('clear')          #clear terminal screen
+
+#Main program
 print("Hello world! This program is running!! :)")
 
 name = input("What's your name my friend? ")
